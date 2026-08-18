2e0df542ba1907aa4f5d55f872a08ae9bae978ef
Somasundar G <somasundar548@gmail.com>
2026-08-18 13:46:47 +0530

    Time: 5 ms (5.29%), Space: 44 MB (36.5%) - LeetHub

---
+++ 3174-clear-digits/3174-clear-digits.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String clearDigits(String s) {
+        Stack<Character> st = new Stack<>();
+        for(char ch : s.toCharArray()){
+            if(ch >= 'a' && ch <= 'z'){
+                st.push(ch);
+            }
+            else{
+                st.pop();
+            }
+        }
+        String res = "";
+        for(char ch : st) res += ch;
+        return res;
+    }
+}
