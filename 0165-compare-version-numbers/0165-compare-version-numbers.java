09cb0f535721303a179681a300aca13e831b708d
Somasundar G <somasundar548@gmail.com>
2026-08-20 13:54:54 +0530

    Time: 1 ms (75.37%), Space: 42.9 MB (60.79%) - LeetHub

---
+++ 0165-compare-version-numbers/0165-compare-version-numbers.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int compareVersion(String version1, String version2) {
+        String [] v1 = version1.split("\\.");
+        String [] v2 = version2.split("\\.");
+        for(int i = 0; i < Math.max(v1.length, v2.length); i++){
+        int a = i < v1.length ? Integer.parseInt(v1[i]) : 0;
+        int b = i < v2.length ? Integer.parseInt(v2[i]) : 0;
+        if(a < b){
+            return -1;
+        }
+        if(a > b){
+            return 1;
+        }
+    }
+        return 0;
+    }
+}
